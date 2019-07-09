ad9e8971ab14c571e49822afa96fed326760aff2
taniko0416 <kouhei1994666@gmail.com>
2019-07-10 00:30:03 +0900

    tmo

--- 05.py
+++ 05.py
@@ -3,4 +3,6 @@
 
 def n_gram(target, n):
   # 基準を1文字(単語)ずつ ずらしながらn文字分抜き出す
-  return [ target[idx:idx + n] for idx in range(len(target) - n + 1)]
+  return [target[idx:idx + n] for idx in range(len(target) - n + 1)]
+  
+  
